b92ab8ac4ac607ca5aac7cd41b7d0b40a2e77768
Gallo Garcia Nicolas <gallogarcianicolas@gmail.com>
2025-02-24 13:53:53 -0300

    fix: corrigiendo estilos QR

--- src/components/QRPayment.tsx
+++ src/components/QRPayment.tsx
@@ -21,7 +21,6 @@ export default function QRPayment({
           bgColor="#FFFFFF" // Asegura un fondo blanco puro
           fgColor="#000000" // Asegura un color de QR negro puro
           level="H" // Usa el nivel de corrección de errores más alto
-          includeMargin={false} // Evita márgenes extra
         />
       ) : (
         <p className="text-sm text-gray-500">QR no disponible</p>
@@ -29,36 +28,3 @@ export default function QRPayment({
     </div>
   );
 }
-
-// "use client";
-
-// import { IGetOrderInfo } from "@/lib/api/types";
-// import { QRCodeCanvas } from "qrcode.react";
-
-// interface QRPaymentProps {
-//   order: IGetOrderInfo;
-//   paymentUri: string | null; // ✅ Ahora puede recibir `payment_uri`
-// }
-
-// export default function QRPayment({ order, paymentUri }: QRPaymentProps) {
-//   console.log("order", order); // ✅ Usamos `clog` en lugar de `console.log`
-//   const qrValue = paymentUri ?? order.address; // ✅ Usamos `paymentUri` primero, luego `order.address`
-
-//   return (
-//     <div className="flex flex-col items-center justify-center p-6 bg-white rounded-lg shadow-md max-w-lg mx-auto">
-//       <h2 className="text-xl font-semibold mb-4 text-gray-900">
-//         Escanea el QR para pagar
-//       </h2>
-
-//       {qrValue ? (
-//         <QRCodeCanvas value={qrValue} size={200} />
-//       ) : (
-//         <p className="text-sm text-gray-500">QR no disponible</p>
-//       )}
-
-//       <p className="mt-4 text-sm text-gray-700">
-//         <strong>Dirección de pago:</strong> {order.address ?? "No disponible"}
-//       </p>
-//     </div>
-//   );
-// }
