339c350ff3425f6445818cb68de3436f58e8f802
Gallo Garcia Nicolas <gallogarcianicolas@gmail.com>
2025-02-24 14:17:20 -0300

    field:agregado footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,10 @@ const Footer = () => {
         <span className="font-bold flex items-center">
           <img src="/asd.png" alt="Bitnovo" className="h-14 w-auto " />
         </span>
-        <span className="text-gray-400">|</span>
+        <svg width="2" height="33" viewBox="0 0 2 40" className="text-gray-400 opacity-50">
+          <rect width="1.6" height="40" fill="currentColor" />
+        </svg>
+
         <span className="text-gray-400">
           &copy; 2022 Bitnovo. All rights reserved.
         </span>
