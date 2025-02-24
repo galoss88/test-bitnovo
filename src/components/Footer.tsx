989e49950b99e9128f3754bd1b904c61eac25b7e
Gallo Garcia Nicolas <gallogarcianicolas@gmail.com>
2025-02-24 14:10:45 -0300

    field:agregado footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center text-gray-400 text-sm py-4 border-t bg-white">
+    <footer className="flex justify-center items-center border-none text-sm py-4  bg-white">
       <div className="flex items-center gap-2">
         <span>Powered by</span>
         <span className="font-bold flex items-center">
-          <img src="/bitnovo-logo.png" alt="Bitnovo" className="h-5 w-auto" />
-          <span className="text-gray-400">Bitnovo.</span>
+          <img src="/asd.png" alt="Bitnovo" className="h-14 w-auto " />
         </span>
         <span className="text-gray-400">|</span>
         <span className="text-gray-400">
