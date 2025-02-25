b76e32ba1f0c85d8b9cf5dcdee7f673bc05e76bc
Gallo Garcia Nicolas <gallogarcianicolas@gmail.com>
2025-02-25 09:03:09 -0300

    fix: pequeña correccion footer

--- src/pages/_layout/Footer.tsx
+++ src/pages/_layout/Footer.tsx
@@ -1,17 +1,20 @@
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center border-none text-sm py-4  bg-white">
-      <div className="flex items-center gap-2">
-        <span>Powered by</span>
-        <span className="font-bold flex items-center">
-          <img src="/asd.png" alt="Bitnovo" className="h-14 w-auto " />
-        </span>
+    <footer className="w-full flex justify-center items-center border-none text-sm py-6 bg-white fixed bottom-0 left-0">
+      <div className="w-full max-w-screen-xl px-4 flex flex-col md:flex-row items-center justify-center gap-2">
+        <div className="flex items-center gap-1">
+          <span>Powered by</span>
+          <span className="font-bold flex items-center">
+            <img src="/asd.png" alt="Bitnovo" className="h-10 md:h-14 w-auto" />
+          </span>
+        </div>
+
         <svg width="2" height="33" viewBox="0 0 2 40" className="text-gray-400 opacity-50">
           <rect width="1.6" height="40" fill="currentColor" />
         </svg>
 
-        <span className="text-gray-400">
-          &copy; 2022 Bitnovo. All rights reserved.
+        <span className="text-gray-400 text-center md:text-left">
+          &copy; {new Date().getFullYear()} Bitnovo. All rights reserved.
         </span>
       </div>
     </footer>
